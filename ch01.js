93abe5b124ed1594fc1f72ce1437f395debdc879
xenosign <xenosign@naver.com>
2024-01-17 16:46:00 +0900

    240117

--- ch01.js
+++ ch01.js
@@ -1,9 +1,9 @@
-function is(x, y) {
-    return (x === y && (x !== 0 || 1 / x === 1 / y)) || (x !== x && y !== y);
-}
+// function is(x, y) {
+//     return (x === y && (x !== 0 || 1 / x === 1 / y)) || (x !== x && y !== y);
+// }
 
-// 일반
-var objectIs = typeof Object.is === 'function' ? Object.is : is;
+// // 일반
+// var objectIs = typeof Object.is === 'function' ? Object.is : is;
 
 // function test1(x, y) {
 //     console.log(1 / x);
@@ -24,8 +24,60 @@ var objectIs = typeof Object.is === 'function' ? Object.is : is;
 // console.log(0 / 0);
 // test2(NaN, 0 / 0);
 
-console.log(1 / NaN === 1 / NaN);
-console.log(NaN === Number.NaN);
-console.log(typeof NaN);
+// console.log(1 / NaN === 1 / NaN);
+// console.log(NaN === Number.NaN);
+// console.log(typeof NaN);
 // console.log(NaN !== 0)
 // console.log(-0 !== 0);
+
+// class Car {
+//   constructor(name) {
+//     this.name = name;
+//   }
+
+//   static hello() {
+//     console.log("안녕하세요!");
+//   }
+
+//   sayName() {
+//     console.log(this.name);
+//   }
+// }
+
+// const hyundaiCar = new Car("hyundai");
+
+// Car.hello();
+// hyundaiCar.sayName();
+// hyundaiCar.hello(); // Err
+
+// var x = 10;
+
+// function foo() {
+//   var x = 100;
+//   console.log(x); // 100
+
+//   function bar() {
+//     var x = 1000;
+//     console.log(x); // 1000
+//   }
+// }
+
+// console.log(x); // 10
+
+// for (var i = 0; i < 5; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, i * 1000);
+// };
+
+// for (var i = 0; i < 5; i++) {
+//   setTimeout((function (sec) {
+//     return function () { console.log(sec); }
+//   })(i), i * 1000)
+// };
+
+for (let i = 0; i < 5; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, i * 1000);
+}
