f264dfec2bd7ddbedc2da11abbcb5a431779a46a
xenosign <xenosign@naver.com>
2024-01-23 15:58:23 +0900

    240123

--- 2w/ch02.js
+++ 2w/ch02.js
@@ -1,9 +1,19 @@
-const obj = {
-  ["test:key"]: 1
+// const obj = {
+//   ["test:key"]: 1
+// }
+
+// const obj2 = {
+//   testKey: 1,
+// }
+
+// console.log(obj, obj2);
+
+let a = 1;
+
+function test(a) {
+  a = 2;
 }
 
-const obj2 = {
-  testKey: 1,
-}
+test(a);
 
-console.log(obj, obj2);
+console.log(a);
