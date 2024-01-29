6111eea2803951e2ed80cd7cfcf3cc9533fdaa60
xenosign <xenosign@naver.com>
2024-01-29 15:33:23 +0900

    240129

--- test2/src/App.js
+++ test2/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import ModuleBtn from "./components/ModuleBtn";
 import SassBtn from "./components/SassBtn";
@@ -6,16 +6,25 @@ import StyledBtn from "./components/StyledBtn";
 import TailwindBtn from "./components/TailwindBtn";
 
 function App() {
-  const [state, setState] = useState(() => {
-    console.log("복잡한 연산");
-    return 0;
-  });
+  const [counter, setCounter] = useState(0);
 
+  useEffect(() => {
+    const addMouseEvent = () => {
+      console.log(counter);
+    }
+
+    window.addEventListener('click', addMouseEvent);
+
+    return () => {
+      console.log('클린업 함수 실행!', counter);
+      window.removeEventListener('click', addMouseEvent);
+    }
+  }, [counter]);
 
   return (
     <>
-      <h1>{state}</h1>
-      <button onClick={() => setState((prev) => prev + 1)}>리렌더링</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>리렌더링</button>
     </>
   );
 }
