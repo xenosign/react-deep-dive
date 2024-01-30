3ffac92669c9d6cdbd077f59cdef68ac01e74b47
xenosign <xenosign@naver.com>
2024-01-30 17:07:59 +0900

    240130

--- test2/src/components/UseContext.jsx
+++ test2/src/components/UseContext.jsx
@@ -1,26 +1,53 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
 const Context = createContext(undefined);
 
+function useMyContext() {
+  const context = useContext(Context);
+  if (context === undefined) throw new Error("Contet 를 받아올 수 없습니다");
+
+  return context;
+}
+
+
+const GrandChildComponent = () => {
+  const { hello } = useMyContext();  
+
+  useEffect(() => {
+    console.log("GrandChildComponent 렌더링");
+  })
+
+  return <h1>{hello}</h1>
+}
+
 const ChildComponent = () => {
-  const value = useContext(Context);
+  useEffect(() => {
+    console.log("ChildComponent 렌더링");
+  })
 
   return (
-    <>
-      {value? value.hello : ''}
-    </>
+    <GrandChildComponent />
   )
 }
 
 export default function UseContext() {
+  const [text, setText] = useState('');
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  }
+
+  useEffect(() => {
+    console.log('UseContext 렌더링');
+  })
+
   return (
-    <>
-      <Context.Provider value={{ hello : 'react' }}>
-        <Context.Provider value={{ hello : 'js' }}>
-          <ChildComponent />
-        </Context.Provider>
+    <>  
+      <input value={text} onChange={handleChange} />    
+      <Context.Provider value={{ hello : text }}>
+        
+        <ChildComponent />
       </Context.Provider>
-    
     </>
   )
 }
