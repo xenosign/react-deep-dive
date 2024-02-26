cdbd56ea10dca4ec1ca8482e4f9b0e564ca2dd77
xenosign <xenosign@naver.com>
2024-02-26 16:26:12 +0900

    240226

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import ForwardRef from "./components/ForwardRef";
+import StaticComponent from "./components/StaticComponent";
 import UseDebugValue from "./components/UseDebugValue";
 import UseImperativeHandle from "./components/UseImperativeHandle";
 import UseReducer from "./components/UseReducer";
@@ -8,7 +9,7 @@ import WithFoo from "./components/WithFoo";
 function App() {
   return (
     <div className="App">
-      <WithFoo />
+      <StaticComponent />
     </div>
   );
 }
