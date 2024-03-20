87c79f42215828f76e898b2611776a9523d88130
kdtTetz <xenosign@naver.com>
2024-03-20 20:21:55 +0900

    Update test.js

--- test.js
+++ test.js
@@ -12,3 +12,42 @@ console.log(Boolean(null));
 (function test() {
   console.log("!");
 })();
+
+const tetzObj = {
+  name: "이효석",
+  age: 40,
+  isOld: true,
+
+  doLecture: () => {
+    console.log("강의!");
+  },
+};
+
+console.log(tetzObj);
+
+const tetzName = "이효석";
+const tetzAge = 40;
+const tetzIsOld = true;
+const doLectureByTetz = () => {
+  console.log("강의!");
+};
+
+console.log(tetzName, tetzAge, tetzIsOld);
+
+const customer1 = {
+  name: "손흥민",
+  age: 31,
+  total: 100000,
+  isBlack: false,
+  purchase: function (money) {
+    this.total += money;
+  },
+};
+const customer2 = { name: "이상혁", age: 27, total: 300000, isBlack: false };
+
+// ...
+
+const customer7777 = { name: "클린스만", age: 59, total: 0, isBlack: true };
+
+customer1.purchase(300);
+console.log(customer1);
