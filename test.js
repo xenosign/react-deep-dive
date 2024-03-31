9dd51d36a258892dbbd7095c9d4e204da06f4e0a
xenosign <xenosign@naver.com>
2024-03-31 10:08:45 +0900

    Update test.js

--- test.js
+++ test.js
@@ -7,47 +7,52 @@
 // console.log(Boolean(null));
 // console.log(Boolean(test));
 
-console.log(Boolean(null));
+// console.log(Boolean(null));
 
-(function test() {
-  console.log("!");
-})();
+// (function test() {
+//   console.log("!");
+// })();
 
-const tetzObj = {
-  name: "이효석",
-  age: 40,
-  isOld: true,
+// const tetzObj = {
+//   name: "이효석",
+//   age: 40,
+//   isOld: true,
 
-  doLecture: () => {
-    console.log("강의!");
-  },
-};
+//   doLecture: () => {
+//     console.log("강의!");
+//   },
+// };
 
-console.log(tetzObj);
+// console.log(tetzObj);
 
-const tetzName = "이효석";
-const tetzAge = 40;
-const tetzIsOld = true;
-const doLectureByTetz = () => {
-  console.log("강의!");
-};
+// const tetzName = "이효석";
+// const tetzAge = 40;
+// const tetzIsOld = true;
+// const doLectureByTetz = () => {
+//   console.log("강의!");
+// };
 
-console.log(tetzName, tetzAge, tetzIsOld);
+// console.log(tetzName, tetzAge, tetzIsOld);
 
-const customer1 = {
-  name: "손흥민",
-  age: 31,
-  total: 100000,
-  isBlack: false,
-  purchase: function (money) {
-    this.total += money;
-  },
-};
-const customer2 = { name: "이상혁", age: 27, total: 300000, isBlack: false };
+// const customer1 = {
+//   name: "손흥민",
+//   age: 31,
+//   total: 100000,
+//   isBlack: false,
+//   purchase: function (money) {
+//     this.total += money;
+//   },
+// };
+// const customer2 = { name: "이상혁", age: 27, total: 300000, isBlack: false };
 
-// ...
+// // ...
 
-const customer7777 = { name: "클린스만", age: 59, total: 0, isBlack: true };
+// const customer7777 = { name: "클린스만", age: 59, total: 0, isBlack: true };
 
-customer1.purchase(300);
-console.log(customer1);
+// customer1.purchase(300);
+// console.log(customer1);
+const arr = [];
+console.log(Boolean(arr))
+console.log(arr[0])
+
+console.log(arr && arr[0])
